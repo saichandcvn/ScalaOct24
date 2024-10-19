afd4a2c934e570b8ba16cb9b000301ec222c15c0
saichandcvn <saichandcvn@gmail.com>
2024-10-19 08:51:34 +0530

    Variables and Loops Practice

--- src/main/scala/ifelse.scala
+++ src/main/scala/ifelse.scala
@@ -1,6 +1,6 @@
 object ifelse {
   def main(args:Array[String]):Unit= {
-    var a = 20;
+    var a = 20
 
     if(a%2==0)
       {
--- src/main/scala/whileloop.scala
+++ src/main/scala/whileloop.scala
@@ -1,11 +1,11 @@
 object whileloop {
   def main(args:Array[String]):Unit= {
-    var a = 1;
+    var a = 1
 
     while(a<=10)
       {
         println(a)
-        a=a+1;
+        a=a+1
       }
   }
 }
