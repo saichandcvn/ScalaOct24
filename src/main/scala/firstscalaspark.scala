bb9a2a4fa964461c6cd766e7276a3d4903782596
saichandcvn <saichandcvn@gmail.com>
2024-10-24 08:06:13 +0530

    Minor Correction

--- src/main/scala/firstscalaspark.scala
+++ src/main/scala/firstscalaspark.scala
@@ -10,8 +10,9 @@ object firstscalaspark {
     val rdd1 = input.flatMap(x => x.split(" "))
     val rdd2 = rdd1.map(x => (x, 1))
     val rdd3 = rdd2.reduceByKey((x, y) => (x + y))
-    rdd3.collect().foreach(println)
-
+    val rdd4 = rdd3.sortBy(x=>x._2,false)
+    //rdd4.collect().foreach(println)
+    rdd4.take(2).foreach(println)
     //scala.io.StdIn.readLine()
 
   }
